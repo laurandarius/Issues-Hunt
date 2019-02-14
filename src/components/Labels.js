8a32c13f0d89805a17f1fa85f07978b31e13fcda
fulin426 <liufam@ms-MacBook-Pro.local>
2019-02-14 01:57:09 -0800

    render label background color fixed

--- src/components/Labels.js
+++ src/components/Labels.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const Labels = (props) => {
-  let labelColor = 'background-color:#';
   const LabelsList = props.labels.map( label =>
     <div
       key={label.id}
-      background-color={label.color}
-      style={{labelColor}}
+      style={{"backgroundColor": "#" + label.color}}
       className="issue-label">
       {label.name}
     </div>
