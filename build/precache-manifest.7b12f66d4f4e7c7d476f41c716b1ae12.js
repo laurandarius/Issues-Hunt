7b378576d647a054cab49919400c1abd2bb0ba10
fulin426 <liufam@ms-MacBook-Pro.local>
2019-04-08 21:38:12 -0700

    heroku build

---
+++ build/precache-manifest.7b12f66d4f4e7c7d476f41c716b1ae12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "838c44849475315c6429",
+    "url": "/static/js/main.838c4484.chunk.js"
+  },
+  {
+    "revision": "1519f28c8197160cba4c",
+    "url": "/static/js/2.1519f28c.chunk.js"
+  },
+  {
+    "revision": "838c44849475315c6429",
+    "url": "/static/css/main.22c188e4.chunk.css"
+  },
+  {
+    "revision": "1519f28c8197160cba4c",
+    "url": "/static/css/2.75ce2e56.chunk.css"
+  },
+  {
+    "revision": "6356aff7bd0cc2aece505f6a451a121f",
+    "url": "/index.html"
+  }
+];
